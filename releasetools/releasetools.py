9fd9cc089845f882ec16a6a22b61da12a0331255
countstarlight <countstarlight@outlook.com>
2016-10-04 20:24:44 +0800

    Remove f2fs from recovery.fstab anf replace releasetools/releasetools.py.
    Signed-off-by: countstarlight <countstarlight@outlook.com>

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,5 +1,5 @@
-# Copyright (C) 2009 The Android Open Source Project
-# Copyright (c) 2011, The Linux Foundation. All rights reserved.
+#
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,85 +12,37 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-"""Emit commands needed for QCOM devices during OTA installation
-(installing the radio image)."""
-
+import hashlib
 import common
 import re
 
-def LoadFilesMap(zip, type):
-  try:
-    data = zip.read("RADIO/filesmap")
-  except KeyError:
-    print "Warning: could not find RADIO/filesmap in %s." % zip
-    data = ""
-  d = {}
-  for line in data.split("\n"):
-    line = line.strip()
-    if not line or line.startswith("#"): continue
-    pieces = line.split()
-    if not (len(pieces) == 2):
-      raise ValueError("malformed filesmap line: \"%s\"" % (line,))
-    d[pieces[0]] = pieces[1]
-  return d
-
-def GetRadioFiles(z):
-  out = {}
-  for info in z.infolist():
-    if info.filename.startswith("RADIO/") and (info.filename.__len__() > len("RADIO/")):
-      fn = "RADIO/" + info.filename[6:]
-      out[fn] = fn
-  return out
-
 def FullOTA_Assertions(info):
-  #TODO: Implement device specific asserstions.
-  return
+    AddBasebandAssertion(info)
+    return
 
 def IncrementalOTA_Assertions(info):
-  #TODO: Implement device specific asserstions.
-  return
-
-def InstallRawImage(image_data, api_version, input_zip, fn, info, filesmap):
-  #fn is in RADIO/* format. Extracting just file name.
-  filename = fn[6:]
-  if api_version >= 3:
-    if filename not in filesmap:
-        return
-    info.script.AppendExtra('package_extract_file("%s", "%s");' % (filename,filesmap[filename]))
-    common.ZipWriteStr(info.output_zip, filename, image_data)
+    AddBasebandAssertion(info)
     return
-  else:
-    print "warning raido-update: no support for api_version less than 3."
 
-def FULLOTA_InstallEnd_MMC(info):
-  files = GetRadioFiles(info.input_zip)
-  if files == {}:
-    print "warning radio-update: no radio image in input target_files; not flashing radio"
+def AddBasebandAssertion(info):
+    android_info = info.input_zip.read("OTA/android-info.txt")
+    m = re.search(r'require\s+version-baseband\s*=\s*(\S+)', android_info)
+    if m:
+        versions = m.group(1).split('|')
+        if len(versions) and '*' not in versions:
+            cmd = 'assert(cancro.verify_baseband(' + ','.join(['"%s"' % baseband for baseband in versions]) + ') == "1");'
+            info.script.AppendExtra(cmd)
     return
-  info.script.UnmountAll()
-  info.script.Print("Writing radio image...")
-  #Load filesmap file
-  filesmap = LoadFilesMap(info.input_zip, info.type)
-  if filesmap == {}:
-      print "warning radio-update: no or invalid filesmap file found. not flashing radio"
-      return
-  for f in files:
-    image_data = info.input_zip.read(f)
-    InstallRawImage(image_data, info.input_version, info.input_zip, f, info, filesmap)
-  return
 
-def FULLOTA_InstallEnd_MTD(info):
-  print "warning radio-update: no implementation for radio upgrade for NAND devices"
-  return
+
+def FullOTA_PostValidate(info):
+    info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/msm_sdcc.1/by-name/system");');
+    info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/platform/msm_sdcc.1/by-name/system");');
+    info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/msm_sdcc.1/by-name/system");');
 
 def FullOTA_InstallEnd(info):
-  if info.type == 'MTD':
-    FULLOTA_InstallEnd_MTD(info)
-  if info.type == 'MMC':
-    FULLOTA_InstallEnd_MMC(info)
-
-def IncrementalOTA_InstallEnd(info):
-  #TODO: Implement device specific asserstions.
-  print "warning radio-update: no real implementation of IncrementalOTA_InstallEnd."
-  return
+    info.script.Mount("/system");
+    info.script.AppendExtra('assert(run_program("/tmp/install/bin/device_check.sh") == 0);');
+    info.script.Unmount("/system");
