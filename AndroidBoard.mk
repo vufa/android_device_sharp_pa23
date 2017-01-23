c8484e0e61615d68e668bdf78b15564a7a26fd21
JerryZheng <jerryzheng310@gmail.com>
2017-01-24 00:23:10 +0800

    pa23: remove useless files

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -7,6 +7,7 @@ ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 #----------------------------------------------------------------------
 # extra images
 #----------------------------------------------------------------------
+include build/core/generate_extra_images.mk
 # include the non-open-source counterpart to this file
 -include vendor/sharp/pa23/AndroidBoardVendor.mk
 
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,24 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2017 The Mokee Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 BOARD_VENDOR := pa23
 
 LOCAL_PATH := device/sharp/pa23
 
-# inherit from the proprietary version
--include vendor/sharp/pa23/BoardConfigVendor.mk
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8974
@@ -19,17 +33,15 @@ USE_CLANG_PLATFORM_BUILD := true
 
 # Architecture
 TARGET_ARCH         := arm
-TARGET_BOOTLOADER_BOARD_NAME := msm8974
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI      := armeabi-v7a
 TARGET_CPU_ABI2     := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT  := generic
-TARGET_HARDWARE     := qcom
-ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_CPU_SMP      := true
+TARGET_CPU_VARIANT  := krait
 
 # Flags
-COMMON_GLOBAL_CFLAGS   += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64 -DUSE_RIL_VERSION_10
-COMMON_GLOBAL_CPPFLAGS += -DUSE_RIL_VERSION_10
+BOARD_GLOBAL_CFLAGS   += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64 -DUSE_RIL_VERSION_10
+BOARD_GLOBAL_CPPFLAGS += -DUSE_RIL_VERSION_10
 
 # Kernel
 BOARD_KERNEL_CMDLINE               := console=none vmalloc=340M androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
@@ -41,27 +53,12 @@ TARGET_KERNEL_SOURCE               := kernel/sharp/msm8974
 TARGET_KERNEL_ARCH                 := arm
 TARGET_KERNEL_CONFIG               := pa23_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin
 BOARD_DTBTOOL_ARGS                 := -2
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE            := true
 TARGET_POWERHAL_VARIANT             := qcom
-TARGET_POWERHAL_SET_INTERACTIVE_EXT := $(LOCAL_PATH)/power/power_ext.c
 
-# Wifi
-BOARD_HAS_QCOM_WLAN              := true
-BOARD_WLAN_DEVICE                := qcwcn
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_FW_PATH_STA          := "sta"
-WIFI_DRIVER_FW_PATH_AP           := "ap"
-TARGET_PROVIDES_WCNSS_QMI        := true
-TARGET_USES_QCOM_WCNSS_QMI       := true
-TARGET_USES_WCNSS_CTRL           := true
 
 # Filesystem
 TARGET_BOOTIMG_SIGNED                := false
@@ -110,9 +107,6 @@ TARGET_NO_RPC := true
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
-# Use HW crypto for ODE
-TARGET_HW_DISK_ENCRYPTION := false
-
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
@@ -144,3 +138,5 @@ include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
         $(LOCAL_PATH)/sepolicy
+-include vendor/sharp/pa23/BoardConfigVendor.mk
+-include vendor/qcom/binaries/msm8974/graphics/BoardConfigVendor.mk
